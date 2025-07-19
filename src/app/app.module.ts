1b1138053c37a3d7f2426e812e92bd070afd8022
bnoa-io <brayanapeles.dev@gmail.com>
2025-07-19 00:38:57 -0300

    feat: colocando imagem na login e botões na listagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { UserListModule } from './user-list/user-list.module';
@@ -27,7 +28,14 @@ const routes: Routes = [
     LoginModule,
     UserListModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic'
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -8,27 +8,28 @@ interface User {
   name: string;
   position: string;
   hiringDate: string;
+  status: boolean;
   salary: number;
 }
 
 const ELEMENT_DATA: User[] = [
-  { name: 'Brayan Apeles', position: 'Administrador', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'João da Silva', position: 'Front-end Developer', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Simão Nunes', position: 'Back-end Developer', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Pedro Oliveira', position: 'Product Owner', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'André Silva', position: 'Quality Assurance', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Tiago Costa', position: 'Quality Assurance', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Felipe Ferreira', position: 'Quality Assurance', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Bartolomeu Dias', position: 'Tech lead', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Tomé Duarte', position: 'Marketing', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Mateus Souza', position: 'Marketing', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Tiago Esperança', position: 'Marketing', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Tadeu Messias', position: 'Financial', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Judas Priest', position: 'Financial', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Ozzy Osborn', position: 'Designer', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Corey Taylor', position: 'Front-end Developer', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Patrícia Carla', position: 'Back-end Developer', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
-  { name: 'Ana Nunes', position: 'Back-end Developer', hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Brayan Apeles', position: 'Administrador', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'João da Silva', position: 'Front-end Developer', status: false, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Simão Nunes', position: 'Back-end Developer', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Pedro Oliveira', position: 'Product Owner', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'André Silva', position: 'Quality Assurance', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Tiago Costa', position: 'Quality Assurance', status: false, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Felipe Ferreira', position: 'Quality Assurance', status: false, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Bartolomeu Dias', position: 'Tech lead', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Tomé Duarte', position: 'Marketing', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Mateus Souza', position: 'Marketing', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Tiago Esperança', position: 'Marketing', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Tadeu Messias', position: 'Financial', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Judas Priest', position: 'Financial', status: false, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Ozzy Osborn', position: 'Designer', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Corey Taylor', position: 'Front-end Developer', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Patrícia Carla', position: 'Back-end Developer', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
+  { name: 'Ana Nunes', position: 'Back-end Developer', status: true, hiringDate: '2025-07-16T15:30:00', salary: 5500 },
 ];
 
 @Component({
@@ -38,7 +39,7 @@ const ELEMENT_DATA: User[] = [
 })
 export class UserListComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource(ELEMENT_DATA);
-  displayedColumns: string[] = ['select', 'name', 'position', 'hiringDate', 'salary', 'action'];
+  displayedColumns: string[] = ['name', 'position', 'hiringDate', 'status', 'salary', 'action'];
   selection = new SelectionModel<User>(true, []);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -49,16 +50,20 @@ export class UserListComponent implements OnInit, AfterViewInit {
   ){}
 
   ngOnInit(): void {
-    this._paginatorService.itemsPerPageLabel = 'Usuários por página:';
-    this._paginatorService.firstPageLabel = "Primeira página";
-    this._paginatorService.lastPageLabel = "Ultima página"
-    this._paginatorService.nextPageLabel = "Próxima página"
-    this._paginatorService.previousPageLabel = "Página anterior";
+    this.dataSource.filterPredicate = function(data: User, filter: string): boolean {
+      return data.name.trim().toLowerCase().includes(filter);
+    };
+    this._initPaginator();
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: KeyboardEvent) {
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    this.dataSource.filter = input.value.trim().toLowerCase();
   }
 
   isAllSelected(): boolean {
@@ -71,5 +76,13 @@ export class UserListComponent implements OnInit, AfterViewInit {
       return;
     }
     this.selection.select(...this.dataSource.data);
+  }
+
+  private _initPaginator(): void {
+    this._paginatorService.itemsPerPageLabel = 'Usuários por página:';
+    this._paginatorService.firstPageLabel = "Primeira página";
+    this._paginatorService.lastPageLabel = "Ultima página"
+    this._paginatorService.nextPageLabel = "Próxima página"
+    this._paginatorService.previousPageLabel = "Página anterior";
   }
 }
